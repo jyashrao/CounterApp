f0e47336142eef26b81d5ac1c8bb7d72d3769055
1010YASH <jyashrao10@gmail.com>
2026-02-02 23:29:59 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const counterValue=document.querySelector('#counter');
+
+const increment = () => {
+    let value = parseInt(counterValue.innerText);
+    value+=1;
+    counterValue.innerText=value;
+};
+
+const decrement = () => {
+    let value = parseInt(counterValue.innerText);
+    value-=1;
+    counterValue.innerText=value;
+};
